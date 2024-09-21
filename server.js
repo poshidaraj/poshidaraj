04a267b6a35491fde41bcadfaccccaf03d26de79
poshidaraj <deenireels@gmail.com>
2024-09-21 15:22:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const CKEDITOR_SCRIPT = '<script src="https://cdn.ckeditor.com/4.16.0/standard/c
 
 // Basic authentication middleware
 const auth = (req, res, next) => {
-    const auth = { login: '790343', password: '123' };
+    const auth = { login: 'arshad', password: 'Quin@143' };
     
     const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
     const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':');
